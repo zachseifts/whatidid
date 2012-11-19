79c941a24b34008c2597581d41bc9957dfe34f80
Zach Seifts <zach.seifts@gmail.com>
2012-11-19 11:11:50 -0500

    updating stuff

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='whatidid',
-      version='1.0',
+      version='1.1',
       description='Keep track of your life',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Life Logging',
+      ],
+      keywords='lifelogging whatidid',
       url='https://github.com/zachseifts/whatidid',
       author='Zach Seifts',
       author_email='zach.seifts@gmail.com',
